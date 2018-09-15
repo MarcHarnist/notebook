491c87c46f51f7d9a00387806e125aea6a29e7c4
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2018-09-15 08:04:20 +0200

    Delete __member-deconnection.php

--- controller/__member-deconnection.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-
-if (isset($_SESSION['member'])){ // If session member exists, restore object
-	unset($member);
-	unset($_SESSION['member']);
-	session_destroy(); // The session is closed
-}
