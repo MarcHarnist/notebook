471e5e621c8c4885bfc7944c9d413830244bcdeb
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2018-09-15 07:45:17 +0200

    Add files via upload
    New models

--- model/class_uploader.php
+++ model/class_uploader.php
@@ -3,5 +3,5 @@
 // This file is required in www.index.php
 
 // Function upload OOP classes if an object is created
-function upload($classname) { require ROOT . 'class/' . $classname.'.php';}
+function upload($classname) { require 'classes/' . $classname.'.php';}
 spl_autoload_register('upload');
