fc3e2db91569e5248c8399559d3a148ad8a0123a
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2018-09-15 07:43:16 +0200

    Delete get-one-page.php
    Replace by methods in the class Database

--- model/get-one-page.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$id = "1"; // default value
-if(isset($_GET['id']) && !empty($_GET['id']))$id = htmlspecialchars($_GET['id']);
-$req = $db->prepare('SELECT * FROM ' . TABLE_PAGES . ' WHERE id = ' . $id . '');
-$req->execute();
-$pages = $req->fetchAll();
-return $pages;
