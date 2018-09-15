84ed86922353c403925b5201beccf55b687d30c2
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2018-09-15 07:41:38 +0200

    Delete USER1.php
    Replace by methods in the class Website

--- model/USER1.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Redirect to home page if not level 1
-if(!$_SESSION['member'] && !$level) {
-	header('Location: ' . WEBSITE_URL);
-} elseif($level > 1) header('Location: ' . WEBSITE_URL);
