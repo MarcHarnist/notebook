6a44683936c611591bac83494da86cae72f02aa0
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2018-09-15 07:41:59 +0200

    Delete USER4.php
    Replace by methods in the class Website

--- model/USER4.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Redirect to home page if not level 1
-if(!$_SESSION['member'] && !$level) {
-	header('Location: ' . WEBSITE_URL);
-} elseif($level <= 4) header('Location: ' . WEBSITE_URL);
